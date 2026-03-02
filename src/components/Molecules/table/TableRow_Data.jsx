fff13af81fe4eb6c7e70d14df9b6ea4830af71a7
Tomasz Seredynski <123777101+MechanikGamer@users.noreply.github.com>
2026-03-02 22:57:40 +0100

    Down to 68

--- src/components/Molecules/table/TableRow_Data.jsx
+++ src/components/Molecules/table/TableRow_Data.jsx
@@ -1,21 +1,28 @@
+import PropTypes from 'prop-types';
 import TableRow from '../../Atoms/Table/Tr/TableRow';
 
-const TableRow_Data = ({
-   TableDataArray = [],
-   TableDataClassName = '',
-   TableRowClassName = '',
-}) => {
-   if (TableDataArray.length <= 0) {
-      return (
-         <h1 className='text-center text-white'>No data Found</h1>
-      );
-   }
+const TableRow_Data = ({ TableDataArray, TableRowClassName }) => {
+  if (!TableDataArray.length) {
+    return <h1 className="text-center text-white">No data Found</h1>;
+  }
 
-   return (
-      <TableRow Tailwind_utility_Class={TableRowClassName}>
-         {TableDataArray.map((item, index) => ({ data }))}
-      </TableRow>
-   );
+  return (
+    <TableRow Tailwind_utility_Class={TableRowClassName}>
+      {TableDataArray.map((node, index) => (
+        <span key={index}>{node}</span>
+      ))}
+    </TableRow>
+  );
+};
+
+TableRow_Data.propTypes = {
+  TableDataArray: PropTypes.arrayOf(PropTypes.node),
+  TableRowClassName: PropTypes.string,
+};
+
+TableRow_Data.defaultProps = {
+  TableDataArray: [],
+  TableRowClassName: '',
 };
 
 export default TableRow_Data;
