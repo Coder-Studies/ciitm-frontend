0e6c5da55aac18df5eed574518db22e86131de03
Tomasz Seredynski <123777101+MechanikGamer@users.noreply.github.com>
2026-03-02 22:16:53 +0100

    down to 188

--- src/components/Templates/Admin/form/FormTemplate.jsx
+++ src/components/Templates/Admin/form/FormTemplate.jsx
@@ -7,22 +7,23 @@ import {
    increaseParPage,
    decreaseParPage,
 } from '../../../../store/NavigatorSlice';
+import PropTypes from 'prop-types';
 
-const FormTemplate = ({ children, Navigator = true, PageName }) => {
-   let dispatch = useDispatch();
-
+const FormTemplate = ({ children, Navigator: showNavigator = true, PageName }) => {
+   const dispatch = useDispatch();
    const navigator = useSelector(state => state.Navigator.navigator);
 
-   let findNavigator = navigator.find(
-      item => item.pageName === PageName,
-   );
+   const findNavigator = navigator.find(item => item.pageName === PageName);
+
+   const canNavigate = showNavigator && !!findNavigator;
 
    return (
-      <div className='max-[553px]:shadow-lg max-[553px]:shadow-blue-700  w-[94%] max-[553px]:w-[87%] h-[90%] flex flex-col items-center  bg-[#1C1C1C] justify-between rounded-bl-2xl rounded-br-2xl mb-[18vh] '>
-         <table className='w-full h-auto bg-[#1C1C1C]  table-fixed '>
+      <div className='max-[553px]:shadow-lg max-[553px]:shadow-blue-700 w-[94%] max-[553px]:w-[87%] h-[90%] flex flex-col items-center bg-[#1C1C1C] justify-between rounded-bl-2xl rounded-br-2xl mb-[18vh]'>
+         <table className='w-full h-auto bg-[#1C1C1C] table-fixed'>
             {children}
          </table>
-         {Navigator && (
+
+         {canNavigate && (
             <div className='w-full h-[8vh] bg-[#090909] rounded-bl-2xl rounded-br-2xl text-white flex items-center gap-[1.5vw] justify-end text-[1.5vw]'>
                <FaArrowAltCircleLeft
                   onClick={() => {
@@ -35,6 +36,7 @@ const FormTemplate = ({ children, Navigator = true, PageName }) => {
                      );
                   }}
                />
+
                <FaArrowAltCircleRight
                   className='mr-[2vw]'
                   onClick={() => {
@@ -51,6 +53,16 @@ const FormTemplate = ({ children, Navigator = true, PageName }) => {
          )}
       </div>
    );
+};
+
+FormTemplate.propTypes = {
+   children: PropTypes.node.isRequired,
+   Navigator: PropTypes.bool,
+   PageName: PropTypes.string.isRequired,
+};
+
+FormTemplate.defaultProps = {
+   Navigator: true,
 };
 
 export default FormTemplate;
